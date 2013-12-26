763bf2fe723cff5ce1bf9aa73cf08765553cf9f7
Abdulsattar Mohammed <asattar.md@gmail.com>
2013-12-26 11:21:10 +0530

    Added javascript

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById("name").addEventListener("keyup", function(e) {
+  console.log('validating')
+  if(document.getElementById("name").value == null || document.getElementById("name").value == '') {
+    document.getElementById("name-error").innerText = "Name is required."
+    e.preventDefault();
+  } else {
+    document.getElementById("name-error").innerText = ""
+  }
+}, false);
