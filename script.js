dbc2163010d25886ac34aebbddf4e9f2043199cd
hashmi-smf <hashmi.smf@outlook.com>
2014-05-09 13:00:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-document.getElementById("name").addEventListener("keyup", function(e) {
-  console.log('validating')
+document.getElementById("name").addEventListener("keyup", function(e) {  console.log('validating')
   if(document.getElementById("name").value == null || document.getElementById("name").value == '') {
     document.getElementById("name-error").innerText = "Name is required."
     e.preventDefault();
